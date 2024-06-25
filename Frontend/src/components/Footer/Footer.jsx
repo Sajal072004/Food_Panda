19b7ff7d30725d3f867610d89f2e61a685179c96
PrathamDwivedi27 <pratham.gungun12345@gmail.com>
2024-06-25 12:54:07 +0530

    Completed the frontend part

--- Frontend/src/components/Footer/Footer.jsx
+++ Frontend/src/components/Footer/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
     <div className='footer' id="footer">
         <div className="footer-content">
             <div className="footer-content-left">
-                <img src={assets.logo} alt="" />
+                <img className="footer-logo-img"src={assets.logo} alt="" width="50%" height="45%"  />
                 <p>Delivering deliciousness right to your door. Our mission is to connect you with the best local restaurants and provide a seamless, mouth-watering experience. We pride ourselves on fast, reliable service and a diverse selection of cuisines to satisfy every craving. Your satisfaction is our priority.</p>
                 <div className="footer-social-icons">
                     <img src={assets.facebook_icon} alt="" />
@@ -15,7 +15,7 @@ const Footer = () => {
                 </div>
             </div>
 
-            <div className="food-content-center">
+            <div className="footer-content-center">
                 <h2>COMPANY</h2>
                 <ul>
                     <li>Home</li>
--- Frontend/src/components/Navbar/Navbar.jsx
+++ Frontend/src/components/Navbar/Navbar.jsx
@@ -1,23 +1,33 @@
 import './Navbar.css'
 import {assets} from '../../assets/assets.js'
+import {Link} from 'react-router-dom'
+import { useContext } from 'react'
+import { StoreContext } from '../../context/StoreContext.jsx'
 
-const Navbar = () => {
+const Navbar = ({setShowLogin}) => {
+
+  const {getTotalCartAmount  }=useContext(StoreContext);
   return (
       <div className="navbar">
-        <img src={assets.logo} alt="" className='logo' />
+        <Link to='/'><img src={assets.logo} alt="" className='logo' /></Link>
         <ul className="navbar-menu">
-            <li>Home</li>
-            <li>Menu</li>
-            <li>Mobile-app</li>
-            <li>contact us</li>
+            <Link to='/'>Home</Link>
+            <a href='#explore-menu'>Menu</a>
+            <a href='#app-download'>Mobile-app</a>
+            <a href='#footer'>contact us</a>
         </ul>
         <div className="navbar-right">
             <img src={assets.search_icon} alt="" />
             <div className="navbar-search-icon">
-                <img src={assets.basket_icon} alt="" />
-                <div className="dot"></div>
+                <Link to='/cart'><img src={assets.basket_icon} alt="" /></Link>
+                <div className={
+                  getTotalCartAmount()===0
+                  ?""
+                  :"dot"
+
+                }></div>
             </div>
-            <button>sign in </button>
+            <button onClick={()=>setShowLogin(true)}>sign in </button>
         </div>
       </div>
   )
