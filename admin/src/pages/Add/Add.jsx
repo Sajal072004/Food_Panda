0df7e5ea8ff220f8cf5f13b7a38b5b10ef5706ba
Sajal Namdeo <113107248+Sajal072004@users.noreply.github.com>
2024-07-02 02:58:07 +0530

    Update Add.jsx

--- admin/src/pages/Add/Add.jsx
+++ admin/src/pages/Add/Add.jsx
@@ -7,7 +7,7 @@ import { toast } from 'react-toastify';
 
 const Add = () => {
 
-    const url='http://localhost:4000';
+    const url='https://food-panda-backend.onrender.com';
     const [image,setImage]=useState(false);
     const [data,setData]=useState({
         name:"",
