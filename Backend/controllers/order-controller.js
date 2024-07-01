5db8e335f3518f188821e5e619d6e6ea2ec5bc79
Sajal Namdeo <113107248+Sajal072004@users.noreply.github.com>
2024-07-02 02:46:25 +0530

    Update order-controller.js

--- Backend/controllers/order-controller.js
+++ Backend/controllers/order-controller.js
@@ -8,7 +8,7 @@ const stripe=new Stripe(process.env.STRIPE_SECRET_KEY);
 
 const placeOrder=async (req,res)=>{
 
-    const frontend_url="http://localhost:5173";
+    const frontend_url="https://food-panda-dviw.onrender.com";
     console.log(req.body.userId);
     try {
         const newOrder=new orderModel({
