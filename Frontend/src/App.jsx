487e13a515ae1de019c540f00a5c49d63a434883
Sajal Namdeo <113107248+Sajal072004@users.noreply.github.com>
2024-07-02 15:11:46 +0530

    Update App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -4,7 +4,7 @@ import Home from "./pages/Home/Home.jsx";
 import Cart from "./pages/Cart/Cart.jsx";
 import PlaceOrder from "./pages/PlaceOrder/PlaceOrder.jsx";
 import Footer from "./components/Footer/Footer.jsx";
-import { useState } from "react";
+import { useState , useEffect } from "react";
 import LoginPopup from "./components/LoginPopup/LoginPopup.jsx";
 import Verify from '../src/pages/Verify/Verify.jsx';
 import MyOrders from "./pages/MyOrders/MyOrders.jsx";
@@ -15,10 +15,10 @@ const App = () => {
   const[showLogin, setShowLogin]=useState(false);
   useEffect(() => {
     const interval = setInterval(() => {
-      if (window.location.href.startsWith("https://food-delivery-frontend-no0l.onrender.com/verify?success")) {
+      if (window.location.href.startsWith("https://food-panda-dviw.onrender.com/verify?success")) {
         clearInterval(interval); 
         alert("Payment Successful! Check your orders section.");
-        window.location.href = "https://food-delivery-frontend-no0l.onrender.com/";
+        window.location.href = "https://food-panda-dviw.onrender.com/";
       }
     }, 1000); 
 
